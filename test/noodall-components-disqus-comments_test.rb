6001fc12a5886ac1b8141f99c25dc22ac48f5b15
Jordan Elver <jordan.elver@gmail.com>
2012-07-05 16:59:52 +0100

    Remove unused test cruft

--- test/noodall-components-disqus-comments_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class NoodallComponentsDisqusCommentsTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, NoodallComponentsDisqusComments
-  end
-end
